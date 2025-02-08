77ce1309f8edcced61d61a674ced880ffd7510be
newmo13 <kubalipka123@gmail.com>
2025-02-08 03:52:02 +0100

    initial commit

---
+++ src/main/java/dev/jlipka/swiftrestapi/SwiftRestApiApplication.java
@@ -0,0 +1,13 @@
+package dev.jlipka.swiftrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwiftRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/jlipka/swiftrestapi/SwiftRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jlipka.swiftrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

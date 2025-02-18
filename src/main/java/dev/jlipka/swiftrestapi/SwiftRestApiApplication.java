77ce1172bd794abe2f21ae84c49f0fc38ecf38d5
newmo13 <kubalipka123@gmail.com>
2025-02-18 23:56:16 +0100

    cleaned main application class

--- src/main/java/dev/jlipka/swiftrestapi/SwiftRestApiApplication.java
+++ src/main/java/dev/jlipka/swiftrestapi/SwiftRestApiApplication.java
@@ -1,17 +1,11 @@
 package dev.jlipka.swiftrestapi;
 
-import dev.jlipka.swiftrestapi.repository.BankRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SwiftRestApiApplication {
-    @Autowired
-    BankRepository bankRepository;
-
     public static void main(String[] args) {
         SpringApplication.run(SwiftRestApiApplication.class, args);
     }
-
 }

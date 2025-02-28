e844d23d005bb16fc97476f523a75df405185e7c
newmo13 <kubalipka123@gmail.com>
2025-02-28 21:25:19 +0100

    updated return types

--- src/main/java/dev/jlipka/swiftrestapi/api/controller/BankController.java
+++ src/main/java/dev/jlipka/swiftrestapi/api/controller/BankController.java
@@ -1,11 +1,22 @@
 package dev.jlipka.swiftrestapi.api.controller;
 
 
-import dev.jlipka.swiftrestapi.api.dto.*;
+import dev.jlipka.swiftrestapi.api.dto.BankOffice;
+import dev.jlipka.swiftrestapi.api.dto.Branch;
+import dev.jlipka.swiftrestapi.api.dto.CountrySwiftCodes;
 import dev.jlipka.swiftrestapi.service.BankEntityService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/v1/swift-codes")
@@ -18,23 +29,23 @@ public class BankController {
     }
 
     @GetMapping("/{swift-code}")
-    public BankResponseDto getBank(@PathVariable("swift-code") String swiftCode) {
+    public BankOffice getBank(@PathVariable("swift-code") String swiftCode) {
         return bankEntityService.findBySwiftCode(swiftCode);
     }
 
     @GetMapping("/country/{countryISO2code}")
-    public CountryWithBanksResponseDto getCountryBanks(@PathVariable String countryISO2code) {
+    public CountrySwiftCodes getCountryBanks(@PathVariable String countryISO2code) {
         return bankEntityService.findByCountryCode(countryISO2code);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public CrudOperationResponseDto registerBank(@Valid  @RequestBody BankFullDetailsDto bankFullDetailsDto) {
+    public Map<String, String> registerBank(@Valid @RequestBody Branch bankFullDetailsDto) {
         return bankEntityService.registerBank(bankFullDetailsDto);
     }
 
     @DeleteMapping("/{swift-code}")
-    public CrudOperationResponseDto unregisterBank(@PathVariable("swift-code") String swiftCode) {
+    public Map<String, String> unregisterBank(@PathVariable("swift-code") String swiftCode) {
         return bankEntityService.unregister(swiftCode);
     }
 }

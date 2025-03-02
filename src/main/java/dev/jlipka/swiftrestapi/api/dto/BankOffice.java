b49751e99b1fffb007eaffa424b80319df6959f6
newmo13 <kubalipka123@gmail.com>
2025-03-02 18:24:23 +0100

    added setters

--- src/main/java/dev/jlipka/swiftrestapi/api/dto/BankOffice.java
+++ src/main/java/dev/jlipka/swiftrestapi/api/dto/BankOffice.java
@@ -1,18 +1,22 @@
 package dev.jlipka.swiftrestapi.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @SuperBuilder
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class BankOffice {
     String address;
     String bankName;
+    @JsonProperty("isHeadquarter")
     boolean isHeadquarter;
     @NotBlank(message = "SWIFT code is required")
     String swiftCode;
